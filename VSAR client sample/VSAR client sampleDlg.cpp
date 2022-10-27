f7752fdb770617a731258d711fd719824197a6fa
glim-jbpark <jbpark@iglim.net>
2022-10-27 11:20:40 +0900

    first commit

---
+++ VSAR client sample/VSAR client sampleDlg.cpp
@@ -0,0 +1,275 @@
+
+// VSAR client sampleDlg.cpp : 구현 파일
+//
+
+#include "stdafx.h"
+#include "VSAR client sample.h"
+#include "VSAR client sampleDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CVSARclientsampleDlg 대화 상자
+
+
+
+CVSARclientsampleDlg::CVSARclientsampleDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_VSARCLIENTSAMPLE_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_isDestroy = false;
+	m_isRepeat = false;
+	m_thread = nullptr;
+
+	// TODO: 05: repeat thread
+	m_thread = new std::thread([this]() {
+		while (!m_isDestroy)
+		{
+			sleep(500);
+			while (m_isRepeat)
+			{
+				m_vsar.signal("repeat signal", GVSAR_ALERT);
+
+				sleep(1000 * _SLEEP_SECONDS);
+			}
+		}
+	});
+}
+
+void CVSARclientsampleDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CVSARclientsampleDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_ERROR, &CVSARclientsampleDlg::OnBnClickedBtnError)
+	ON_BN_CLICKED(IDC_BTN_SIGNAL, &CVSARclientsampleDlg::OnBnClickedBtnSignal)
+	ON_BN_CLICKED(IDC_BTN_ERROR5, &CVSARclientsampleDlg::OnBnClickedBtnError5)
+	ON_BN_CLICKED(IDC_BTN_SIGNAL4, &CVSARclientsampleDlg::OnBnClickedBtnSignal4)
+	ON_BN_CLICKED(IDC_BTN_SIGNAL5, &CVSARclientsampleDlg::OnBnClickedBtnSignal5)
+	ON_BN_CLICKED(IDC_BTN_START, &CVSARclientsampleDlg::OnBnClickedBtnStart)
+	ON_BN_CLICKED(IDC_BTN_STOP, &CVSARclientsampleDlg::OnBnClickedBtnStop)
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+
+// CVSARclientsampleDlg 메시지 처리기
+
+BOOL CVSARclientsampleDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CVSARclientsampleDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CVSARclientsampleDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CVSARclientsampleDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CVSARclientsampleDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	/* 
+		TODO: 99: exit application
+
+		1. kill thread
+		2. stop vsar
+	*/
+
+	// 1. kill thread
+	m_isDestroy = true;
+	m_isRepeat = false;
+	m_thread->join();
+	if (m_thread != nullptr)
+	{
+		delete m_thread;
+		m_thread = nullptr;
+	}
+
+	// 2. stop vsar
+	m_vsar.stop();
+}
+
+void CVSARclientsampleDlg::OnBnClickedBtnError()
+{
+	/* 
+		TODO: 06: I/O Error
+		해당 애러는 Vision system에서 체크할 수 있음
+		따라서 VSAR에 restart signal 전송
+	*/
+	m_vsar.signal("IO Error", GVSAR_RESTART);
+}
+
+void CVSARclientsampleDlg::OnBnClickedBtnError5()
+{
+	/*
+		TODO: 10: Memory Error
+
+		access to nullptr
+	*/
+	unsigned char *buffer = nullptr;
+	*buffer = 'a';
+}
+
+void CVSARclientsampleDlg::OnBnClickedBtnSignal()
+{
+	// TODO: 01: Signal "Process"
+
+	m_vsar.signal("process");
+}
+
+void CVSARclientsampleDlg::OnBnClickedBtnSignal4()
+{
+	// TODO: 02: Start repeat
+
+	SetTimer(_VSAR_TIMER, 500, NULL);
+	
+}
+
+void CVSARclientsampleDlg::OnBnClickedBtnSignal5()
+{
+	/*
+		TODO: 03: Stop repeat
+	*/
+	
+	KillTimer(_VSAR_TIMER);
+}
+
+
+void CVSARclientsampleDlg::OnBnClickedBtnStart()
+{
+
+	// TODO: 00: m_vsar start
+	m_vsar.start();
+}
+
+
+void CVSARclientsampleDlg::OnBnClickedBtnStop()
+{
+	// TODO: 11: m_vsar stop
+	m_vsar.stop();
+}
+
+void CVSARclientsampleDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	static unsigned long long index = 0;
+	if (nIDEvent == _VSAR_TIMER)
+	{
+		m_vsar.signal("index " + std::to_string(index++));
+	}
+
+	CDialogEx::OnTimer(nIDEvent);
+}
---
+++ VSAR client sample/VSAR client sampleDlg.h
@@ -0,0 +1,59 @@
+
+// VSAR client sampleDlg.h : 헤더 파일
+//
+
+#pragma once
+#include "gVSAR.h"
+#include <thread>
+
+#define _SLEEP_SECONDS		1
+#define _VSAR_TIMER			1
+
+#pragma comment(lib, "C:\\Program Files\\VSAR\\lib\\gLogger.lib")
+#pragma comment(lib, "C:\\Program Files\\VSAR\\lib\\gThreadpool.lib")
+#pragma comment(lib, "C:\\Program Files\\VSAR\\lib\\gVSAR.lib")
+
+// CVSARclientsampleDlg 대화 상자
+class CVSARclientsampleDlg : public CDialogEx
+{
+private:
+	gVSAR m_vsar;
+
+	bool m_isRepeat;
+	bool m_isDestroy;
+	std::thread *m_thread;
+
+// 생성입니다.
+public:
+	CVSARclientsampleDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_VSARCLIENTSAMPLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnBnClickedBtnError();
+	afx_msg void OnBnClickedBtnSignal();
+	afx_msg void OnBnClickedBtnError5();
+	afx_msg void OnBnClickedBtnSignal4();
+	afx_msg void OnBnClickedBtnSignal5();
+	afx_msg void OnBnClickedBtnStart();
+	afx_msg void OnBnClickedBtnStop();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
